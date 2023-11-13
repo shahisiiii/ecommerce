ea1590b4076f9913c113289c35e79eb05aa92b8c
jabeel shahis ak <jabeelshahisak@gmail.com>
2023-11-13 09:42:12 +0530

    categories and products model done

--- ecommapp/models.py
+++ ecommapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here.]
+
+class Categories(models.Model):
+    Category_name=models.CharField(max_length=100,unique=True)
+    is_active=models.BooleanField(default=True)
+    def __str__(self):
+        return self.Category_name
+    
+class Products(models.Model):
+    product_name=models.CharField(max_length=100,unique=True)
+    Category=models.ForeignKey(Categories,on_delete=models.CASCADE)
+    price=models.PositiveBigIntegerField()
+    Image=models.ImageField(upload_to="images",null=True)
+    description=models.CharField(max_length=250,null=True)
+    def __str__(self):
+        return self.product_name
